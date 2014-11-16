2c37e6f3f9491d259193d825eb09cf935058e8b4
ColonyOne <colonyFrontier@gmail.com>
2014-11-16 02:37:10 -0500

    Added meta tags for mobile responsiveness

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,5 @@
+//This gets the values from the various fields for submitting a post and inserts 
+//them into the object properties
 Template.postSubmit.events({
 	'submit form': function(e) {
 	  e.preventDefault();
@@ -11,6 +13,8 @@ var post = {
   }
 });
 
+/*
+This code includes the google javascript places api everytime the template is rendered*/
 Template.postSubmit.rendered = function(){
 	 var script = document.createElement("script");
 	 script.type = "text/javascript";
